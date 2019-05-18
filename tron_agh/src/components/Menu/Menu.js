b24c4c89c8efb0d61c2efb952b83763d6e09e469
Kamil Borowiec <kborowiec95@gmail.com>
2019-05-18 10:54:26 +0200

    update websocket endpoint

--- tron_agh/src/components/Menu/Menu.js
+++ tron_agh/src/components/Menu/Menu.js
@@ -5,18 +5,34 @@ import './Menu.css';
 
 export default class Menu extends Component {
 
-    sendTo() {
-        console.log("send");
-        let message = "TEST!";
-        this.clientRef.sendMessage('/app/hello', message);
-    }
-
     constructor(props) {
         super(props);
-        this.sendTo = this.sendTo.bind(this);
+        this.sendTo = this.sendMessage.bind(this);
+        this.state = {
+            clientConnected: false,
+            messages: []
+        };
     }
 
+    onMessageReceive = (msg, topic) => {
+        console.log('msg = '+JSON.stringify(msg)+ ' topic = '+JSON.stringify(topic));
+        this.setState(prevState => ({
+            messages: [...prevState.messages, msg]
+        }));
+    };
+
+    sendMessage = (msg, selfMsg) => {
+        try {
+            this.clientRef.sendMessage("/app/all", JSON.stringify(selfMsg));
+            return true;
+        } catch(e) {
+            alert('cannot send message on /app/all');
+            return false;
+        }
+    };
+
     render() {
+        const wsSourceUrl = window.location.protocol + "//" + window.location.host + "/handler";
         return(
             <div className="menu__items">
                 <ul>
@@ -33,10 +49,15 @@ export default class Menu extends Component {
                     </li>
                 </ul>
                 <button onClick={this.sendTo}>SEND</button>
-                <SockJsClient url={window.location.protocol + "//" + window.location.host + "/gs-guide-websocket"}
-                              topics={['/topic/greetings']}
-                              onMessage={(msg) => { console.log(msg); }}
-                              ref={ (client) => { this.clientRef = client }} />
+                <SockJsClient url={ wsSourceUrl } topics={["/topic/all"]}
+                              onMessage={ this.onMessageReceive } ref={ (client) => { this.clientRef = client }}
+                              onConnect={ () => {
+                                  console.log('CONNECTED!');
+                                  this.setState({clientConnected: true }) } }
+                              onDisconnect={ () => {
+                                  console.log('DISCONNECTED!');
+                                  this.setState({ clientConnected: false }) } }
+                              debug={ false }/>
             </div>
         )
     }
